e32d0b7f44dcc838b267ae7d7408149b0fa722a1
Sam Odum <sam.odum1@gmail.com>
2019-11-19 22:19:51 +0100

    chore(index): initialize project index page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
